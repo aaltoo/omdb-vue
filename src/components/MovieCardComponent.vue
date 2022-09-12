12234681d6d3f4329679fa45092a6c34464a21ac
aaltoo <scotlandfjord@protonmail.com>
2022-09-12 15:18:58 +0900

    Add movie card component and windicss

---
+++ src/components/MovieCardComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <router-link
+    :to="{ name: 'MovieDetails', params: { id: movie.imdbID } }"
+    class="card w-1/3 lg:w-1/4 flex flex-col justify-center items-center"
+  >
+    <img
+      :src="movie.Poster"
+      :alt="movie.Title + 'poster'"
+      class="h-full w-full object-cover"
+    />
+    <h2>{{ movie.Title }}</h2>
+  </router-link>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { Movie } from "../models/movie";
+defineProps<{
+  movie: Movie;
+}>();
+</script>
+
+<style scoped>
+.card {
+  @apply rounded-xl shadow-lg overflow-hidden;
+  transition-duration: 500ms;
+}
+
+.card:hover {
+  @apply shadow-2xl;
+  @apply transform scale-110;
+  @apply transform translate-z-20;
+  @apply transform rotate-5;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,25 @@
 <template>
-  <input type="text" v-model="searchString" @change="search()" />
-  <div v-for="result in results">
-    <h1>{{ result.Title }}</h1>
+  <div class="w-full h-full flex flex-col justify-center items-center">
+    <input
+      type="text"
+      v-model="searchString"
+      @change="search()"
+      class="my-10 w-1/2 lg:w-1/4 bg-gray-200 rounded-full p-2 border-2 border-gray-300 focus:outline-none focus:border-blue-500"
+    />
+    <div class="flex flex-wrap justify-center w-10/12 gap-15">
+      <MovieCardComponent v-for="result in results" :movie="result">
+      </MovieCardComponent>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 import { httpClient } from "../api/http-client";
+import MovieCardComponent from "../components/MovieCardComponent.vue";
 
 const results: any = ref([]);
 const searchString = ref("");
-
-httpClient.get("?s=drive").then((response) => {
-  results.value = response.data.Search;
-});
 
 function search() {
   httpClient.get(`?s=${searchString.value}`).then((response) => {
